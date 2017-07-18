2d6269e839d7b096676aca4ee813352e21598fac
Serena Ivaldi <serena.ivaldi@inria.fr>
2017-07-18 16:18:35 +0200

    organisation of scripts, ini files, gazebo models in a structured way that can be installed using the iCubContrib methods - will go in contexts

---
+++ App/scripts/geomagic.sh
@@ -0,0 +1,4 @@
+export GTDD_HOME=/opt/geomagic_touch_device_driver
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/geomagic_touch_device_driver/lib
+export QT_PLUGIN_PATH=/opt/geomagic_touch_device_driver/lib/plugins
+export LC_NUMERIC=en_US.UTF-8
